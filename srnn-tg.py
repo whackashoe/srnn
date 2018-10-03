230a74a1ecaa7fa9da734bd1825dfea969c4e496
Jett <whackashoe@gmail.com>
2018-10-03 14:42:19 -0500

    ok

--- srnn-tg.py
+++ srnn-tg.py
@@ -16,7 +16,7 @@ from keras.utils.np_utils import to_categorical
 from keras.preprocessing.text import Tokenizer, text_to_word_sequence
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Model
-from keras.layers import Input, Embedding, GRU, TimeDistributed, Dense
+from keras.layers import Input, Embedding, CuDNNGRU, TimeDistributed, Dense
 
 
 
@@ -44,7 +44,7 @@ for i in range(0, n_chars - seq_length, 1):
     next_chars.append(next_char)
     if i % 10000 == 0:
         pb.print_progress_bar((i / (n_chars - seq_length)) * 100)
-print()
+pb.print_progress_bar(100)
 
 df = pd.DataFrame({'text': texts, 'next_char': next_chars})
 print("Total Patterns: {}".format(df.shape[0]))
@@ -56,10 +56,10 @@ X = df.text.values
 
 #set hyper parameters
 MAX_NUM_WORDS = 30000
-EMBEDDING_DIM = 200
+EMBEDDING_DIM = 512
 VALIDATION_SPLIT = 0.1
 TEST_SPLIT=0.1
-NUM_FILTERS = 50
+NUM_FILTERS = 1024
 MAX_LEN = 512
 Batch_size = 100
 EPOCHS = 10
@@ -100,42 +100,29 @@ x_val_padded_seqs = pad_sequences(x_val_word_ids, maxlen=MAX_LEN)
 print('Padded Sequences')
 
 #slice sequences into many subsequences
-pb = ProgressBar(total=100, prefix='Slice Test')
-x_test_padded_seqs_split=[]
-for i in range(x_test_padded_seqs.shape[0]):
-    split1=np.split(x_test_padded_seqs[i],8)
-    a=[]
-    for j in range(8):
-        s=np.split(split1[j],8)
-        a.append(s)
-    x_test_padded_seqs_split.append(a)
-    pb.print_progress_bar((i / x_test_padded_seqs.shape[0]) * 100)
-print()
+def slice_seq(progress_prefix, padded_seqs):
+    pb = ProgressBar(total=100, prefix=progress_prefix)
+    seqs_split=[]
 
-pb = ProgressBar(total=100, prefix='Slice Val')
-x_val_padded_seqs_split=[]
-for i in range(x_val_padded_seqs.shape[0]):
-    split1=np.split(x_val_padded_seqs[i],8)
-    a=[]
-    for j in range(8):
-        s=np.split(split1[j],8)
-        a.append(s)
-    x_val_padded_seqs_split.append(a)
-    pb.print_progress_bar((i / x_val_padded_seqs.shape[0]) * 100)
-print()
+    for i in range(padded_seqs.shape[0]):
+        splitted = np.split(padded_seqs[i],8)
+        a=[]
+
+        for j in range(8):
+            s = np.split(splitted[j],8)
+            a.append(s)
+
+        seqs_split.append(a)
+        if i % 1000 == 0:
+            pb.print_progress_bar((i / padded_seqs.shape[0]) * 100)
+
+    pb.print_progress_bar(100)
+    return seqs_split
+
+x_test_padded_seqs_split=slice_seq('Slice Test', x_test_padded_seqs)
+x_val_padded_seqs_split=slice_seq('Slice Val', x_val_padded_seqs)
+x_train_padded_seqs_split=slice_seq('Slice Train', x_train_padded_seqs)
    
-pb = ProgressBar(total=100, prefix='Slice Train')
-x_train_padded_seqs_split=[]
-for i in range(x_train_padded_seqs.shape[0]):
-    split1=np.split(x_train_padded_seqs[i],8)
-    a=[]
-    for j in range(8):
-        s=np.split(split1[j],8)
-        a.append(s)
-    x_train_padded_seqs_split.append(a)
-    pb.print_progress_bar((i / x_train_padded_seqs.shape[0]) * 100)
-print()
-
 embedding_layer = Embedding(MAX_NUM_WORDS + 1,
                             EMBEDDING_DIM,
                             weights=[np.random.random((MAX_NUM_WORDS + 1, EMBEDDING_DIM))],
@@ -146,17 +133,17 @@ embedding_layer = Embedding(MAX_NUM_WORDS + 1,
 print("Build Model")
 input1 = Input(shape=(MAX_LEN//64,), dtype='int32')
 embed = embedding_layer(input1)
-gru1 = GRU(NUM_FILTERS,recurrent_activation='sigmoid',activation=None,return_sequences=False)(embed)
+gru1 = CuDNNGRU(NUM_FILTERS,return_sequences=False)(embed)
 Encoder1 = Model(input1, gru1)
 
 input2 = Input(shape=(8,MAX_LEN//64,), dtype='int32')
 embed2 = TimeDistributed(Encoder1)(input2)
-gru2 = GRU(NUM_FILTERS,recurrent_activation='sigmoid',activation=None,return_sequences=False)(embed2)
+gru2 = CuDNNGRU(NUM_FILTERS,return_sequences=False)(embed2)
 Encoder2 = Model(input2,gru2)
 
 input3 = Input(shape=(8,8,MAX_LEN//64), dtype='int32')
 embed3 = TimeDistributed(Encoder2)(input3)
-gru3 = GRU(NUM_FILTERS,recurrent_activation='sigmoid',activation=None,return_sequences=False)(embed3)
+gru3 = CuDNNGRU(NUM_FILTERS,return_sequences=False)(embed3)
 preds = Dense(n_vocab, activation='softmax')(gru3)
 model = Model(input3, preds)
 
@@ -189,3 +176,7 @@ model.fit(np.array(x_train_padded_seqs_split), y_train,
 from keras.models import load_model
 best_model= load_model(savebestmodel)          
 print(best_model.evaluate(np.array(x_test_padded_seqs_split),y_test,batch_size=Batch_size))
+
+pred = model.predict([x_train_padded_seqs_split], verbose=False)[0]
+print(pred)
+print(sum(star*prob for star, prob in zip([1,2,3,4,5], pred)))
