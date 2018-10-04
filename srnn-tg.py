b5bd0af8b9514f8b0d9d0db9f50217ab1e3efa6c
Jett <whackashoe@gmail.com>
2018-10-04 08:30:01 -0500

    ok

--- srnn-tg.py
+++ srnn-tg.py
@@ -8,72 +8,92 @@ The code is written in keras, using tensorflow backend. We implement the SRNN(8,
 If you have any question, please contact me at zepingyu@foxmail.com.
 '''
 
+import argparse
+import os
+import time
+
 import pandas as pd
 import numpy as np
 from console_progressbar import ProgressBar
 
+import tensorflow as tf
+from tensorflow import keras
+
 from keras.utils.np_utils import to_categorical
-from keras.preprocessing.text import Tokenizer, text_to_word_sequence
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Model
-from keras.layers import Input, Embedding, CuDNNGRU, TimeDistributed, Dense
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Embedding, CuDNNGRU, TimeDistributed, Dense
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import ModelCheckpoint             
 
 
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--input', type=str, required=True, help='')
+parser.add_argument('--output', type=str, required=True, help='')
+parser.add_argument('--max_num_words', type=int, default=256, help='')
+parser.add_argument('--embedding-dim', type=int, default=512, help='')
+parser.add_argument('--slice-width', type=int, default=8, help='')
+parser.add_argument('--validation-split', type=float, default=0.1, help='')
+parser.add_argument('--test-split', type=float, default=0.1, help='')
+parser.add_argument('--max_len', type=int, default=512, help='')
+parser.add_argument('--num-filters', type=int, default=1024, help='')
+parser.add_argument('--batch-size', type=int, default=100, help='')
+parser.add_argument('--epochs', type=int, default=10, help='')
 
-raw_text = open('tinyshakespeare.txt').read().lower()
-chars = sorted(list(set(raw_text)))
-char_to_int = dict((c, i) for i, c in enumerate(chars))
+parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+parser.add_argument('--beta_1', type=float, default=0.9, help='')
+parser.add_argument('--beta_2', type=float, default=0.999, help='')
+parser.add_argument('--epsilon', type=float, default=1e-08, help='')
+args = parser.parse_args()
+
+
+raw_text = open(args.input).read().lower()
 n_chars = len(raw_text)
-n_vocab = len(chars)
 print("Total Characters: {}".format(n_chars))
+
+
+#use tokenizer to build vocab
+tokenizer = Tokenizer(num_words=args.max_num_words, char_level=True)
+tokenizer.fit_on_texts(raw_text)
+vocab = tokenizer.word_index
+n_vocab = len(vocab)
 print("Total Vocab: {}".format(n_vocab))
 
 texts = []
 next_chars = []
 
-seq_length = 100
 pb = ProgressBar(total=100, prefix='Generating Sequences')
-for i in range(0, n_chars - seq_length, 1):
-    seq_in = raw_text[i:i + seq_length]
-    seq_out = raw_text[i + seq_length]
-    enc_text = [char_to_int[char] for char in seq_in]
-    next_char = char_to_int[seq_out]
-    #next_char = seq_out
-    #df.append({'text': enc_text, 'next_char': next_char}, ignore_index=True)
-    texts.append(seq_in)
-    next_chars.append(next_char)
+for i in range(0, n_chars - args.max_len, args.max_len):
+    texts.append(raw_text[i:i + args.max_len])
+    next_chars.append(vocab[raw_text[i + args.max_len]])
+
     if i % 10000 == 0:
-        pb.print_progress_bar((i / (n_chars - seq_length)) * 100)
+        pb.print_progress_bar((i / (n_chars - args.max_len)) * 100)
 pb.print_progress_bar(100)
 
-df = pd.DataFrame({'text': texts, 'next_char': next_chars})
-print("Total Patterns: {}".format(df.shape[0]))
+df = pd.DataFrame({
+    'text':      texts,
+    'next_char': next_chars
+})
+print("Total Sequences: {}".format(df.shape[0]))
 
 
-Y = df.next_char.values
+Y = df.next_char.values-1
 Y = to_categorical(Y, num_classes=n_vocab)
 X = df.text.values
 
-#set hyper parameters
-MAX_NUM_WORDS = 30000
-EMBEDDING_DIM = 512
-VALIDATION_SPLIT = 0.1
-TEST_SPLIT=0.1
-NUM_FILTERS = 1024
-MAX_LEN = 512
-Batch_size = 100
-EPOCHS = 10
 
 #shuffle the data
-indices = np.arange(X.shape[0])
-np.random.seed(2018)
-np.random.shuffle(indices)
-X=X[indices]
-Y=Y[indices]
+#indices = np.arange(X.shape[0])
+#np.random.seed(2018)
+#np.random.shuffle(indices)
+#X=X[indices]
+#Y=Y[indices]
 
 #training set, validation set and testing set
-nb_validation_samples_val = int((VALIDATION_SPLIT + TEST_SPLIT) * X.shape[0])
-nb_validation_samples_test = int(TEST_SPLIT * X.shape[0])
+nb_validation_samples_val = int((args.validation_split + args.test_split) * X.shape[0])
+nb_validation_samples_test = int(args.test_split * X.shape[0])
 
 x_train = X[:-nb_validation_samples_val]
 y_train = Y[:-nb_validation_samples_val]
@@ -82,68 +102,70 @@ y_val =  Y[-nb_validation_samples_val:-nb_validation_samples_test]
 x_test = X[-nb_validation_samples_test:]
 y_test = Y[-nb_validation_samples_test:]
 
-print('Tokenizing')
-#use tokenizer to build vocab
-tokenizer1 = Tokenizer(num_words=MAX_NUM_WORDS, char_level=True)
-tokenizer1.fit_on_texts(df.text)
-vocab = tokenizer1.word_index
-print('Tokenizer fit')
-x_train_word_ids = tokenizer1.texts_to_sequences(x_train)
-x_test_word_ids = tokenizer1.texts_to_sequences(x_test)
-x_val_word_ids = tokenizer1.texts_to_sequences(x_val)
-print('Generated word ids')
+x_train_seqs = tokenizer.texts_to_sequences(x_train)
+x_test_seqs = tokenizer.texts_to_sequences(x_test)
+x_val_seqs = tokenizer.texts_to_sequences(x_val)
+print('Tokenized sequences')
 
 #pad sequences into the same length
-x_train_padded_seqs = pad_sequences(x_train_word_ids, maxlen=MAX_LEN)
-x_test_padded_seqs = pad_sequences(x_test_word_ids, maxlen=MAX_LEN)
-x_val_padded_seqs = pad_sequences(x_val_word_ids, maxlen=MAX_LEN)
+x_train_padded_seqs = pad_sequences(x_train_seqs, maxlen=args.max_len)
+x_test_padded_seqs = pad_sequences(x_test_seqs, maxlen=args.max_len)
+x_val_padded_seqs = pad_sequences(x_val_seqs, maxlen=args.max_len)
 print('Padded Sequences')
 
 #slice sequences into many subsequences
 def slice_seq(progress_prefix, padded_seqs):
     pb = ProgressBar(total=100, prefix=progress_prefix)
-    seqs_split=[]
+    ret = []
 
     for i in range(padded_seqs.shape[0]):
         splitted = np.split(padded_seqs[i],8)
-        a=[]
+        a = []
 
         for j in range(8):
-            s = np.split(splitted[j],8)
-            a.append(s)
+            b = np.split(splitted[j],8)
+            a.append(b)
 
-        seqs_split.append(a)
+        ret.append(a)
         if i % 1000 == 0:
             pb.print_progress_bar((i / padded_seqs.shape[0]) * 100)
 
     pb.print_progress_bar(100)
-    return seqs_split
+    return ret
 
 x_test_padded_seqs_split=slice_seq('Slice Test', x_test_padded_seqs)
 x_val_padded_seqs_split=slice_seq('Slice Val', x_val_padded_seqs)
 x_train_padded_seqs_split=slice_seq('Slice Train', x_train_padded_seqs)
    
-embedding_layer = Embedding(MAX_NUM_WORDS + 1,
-                            EMBEDDING_DIM,
-                            weights=[np.random.random((MAX_NUM_WORDS + 1, EMBEDDING_DIM))],
-                            input_length=MAX_LEN//64,
+embedding_layer = Embedding(args.max_num_words + 1,
+                            args.embedding_dim,
+                            weights=[np.random.random((args.max_num_words + 1, args.embedding_dim))],
+                            input_length=args.max_len//64,
                             trainable=True)
 
 #build model
 print("Build Model")
-input1 = Input(shape=(MAX_LEN//64,), dtype='int32')
+
+gate_params = {
+    'units':                 args.num_filters,
+    'return_sequences':      False,
+    'return_state':          False,
+    'recurrent_initializer': 'glorot_uniform'
+}
+
+input1 = Input(shape=(args.max_len//64,), dtype='int32')
 embed = embedding_layer(input1)
-gru1 = CuDNNGRU(NUM_FILTERS,return_sequences=False)(embed)
+gru1 = CuDNNGRU(**gate_params)(embed)
 Encoder1 = Model(input1, gru1)
 
-input2 = Input(shape=(8,MAX_LEN//64,), dtype='int32')
+input2 = Input(shape=(8,args.max_len//64,), dtype='int32')
 embed2 = TimeDistributed(Encoder1)(input2)
-gru2 = CuDNNGRU(NUM_FILTERS,return_sequences=False)(embed2)
+gru2 = CuDNNGRU(**gate_params)(embed2)
 Encoder2 = Model(input2,gru2)
 
-input3 = Input(shape=(8,8,MAX_LEN//64), dtype='int32')
+input3 = Input(shape=(8,8,args.max_len//64), dtype='int32')
 embed3 = TimeDistributed(Encoder2)(input3)
-gru3 = CuDNNGRU(NUM_FILTERS,return_sequences=False)(embed3)
+gru3 = CuDNNGRU(**gate_params)(embed3)
 preds = Dense(n_vocab, activation='softmax')(gru3)
 model = Model(input3, preds)
 
@@ -151,31 +173,40 @@ print(Encoder1.summary())
 print(Encoder2.summary())
 print(model.summary())
 
-#use adam optimizer
-from keras.optimizers import Adam
-opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=keras.optimizers.Adam(
+        lr = args.lr,
+        beta_1  = args.beta_1,
+        beta_2  = args.beta_2,
+        epsilon = args.epsilon
+    ),
+    metrics=['acc'])
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=opt,
-              metrics=['acc'])
-
-#save the best model on validation set
-from keras.callbacks import ModelCheckpoint             
-savebestmodel = 'save_model/SRNN(8,2)_yelp2013.h5'
-checkpoint = ModelCheckpoint(savebestmodel, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-callbacks=[checkpoint] 
-             
-model.fit(np.array(x_train_padded_seqs_split), y_train, 
-          validation_data = (np.array(x_val_padded_seqs_split), y_val),
-          epochs = EPOCHS,
-          batch_size = Batch_size,
-          callbacks = callbacks,
-          verbose = 1)
+model.fit(
+    np.array(x_train_padded_seqs_split), y_train, 
+    validation_data = (np.array(x_val_padded_seqs_split), y_val),
+    epochs = args.epochs,
+    batch_size = args.batch_size,
+    callbacks = [
+        keras.callbacks.ModelCheckpoint(
+            args.output,
+            monitor='val_acc',
+            verbose=1,
+            save_best_only=True,
+            mode='max'
+        ),
+        keras.callbacks.TensorBoard(log_dir='./logs')
+    ],
+    verbose = 1)
 
 #use the best model to evaluate on test set
-from keras.models import load_model
-best_model= load_model(savebestmodel)          
-print(best_model.evaluate(np.array(x_test_padded_seqs_split),y_test,batch_size=Batch_size))
+best_model= load_model(args.output)          
+print(best_model.evaluate(
+    np.array(x_test_padded_seqs_split),
+    y_test,
+    batch_size=args.batch_size
+))
 
 pred = model.predict([x_train_padded_seqs_split], verbose=False)[0]
 print(pred)
